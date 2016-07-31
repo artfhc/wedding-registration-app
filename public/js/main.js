2aed12cc01e9aeffbd325309981d5c66bd226545
Craig Chang <craigchang@gmail.com>
2016-07-31 11:43:34 -0700

    hook up voting api to voting page

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,28 @@ $(document).ready(function() {
   $('.carousel').carousel({
   	keyboard: true,
     interval: 5000 //changes the speed
-  })
+  });
+  
+  var $votingForm = $('#voting-form');
+  var $votingCards = $votingForm.find('.voting-card');
+
+  $votingCards.click(function(event) {
+    var chooseVal = $(this).attr('data-choose');
+    $votingForm.find('input[name="choice"]').val(chooseVal);
+
+    // Star UI
+    $votingForm.find('i.fa.fa-star').removeClass('selected');
+    $(this).find('i.fa.fa-star').addClass('selected');
+    
+    //$votingForm.submit();
+
+    $.ajax({
+      method: 'POST',
+      url: '/api/choose',
+      data: $('form').serializeToJSON()
+    }).done(function(msg) {
+      console.log(msg);
+    });
+    
+  });
 });
