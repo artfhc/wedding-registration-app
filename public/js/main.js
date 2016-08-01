ba6681b917eda965200a6663bb0145048b8ee613
Craig Chang <craigchang@gmail.com>
2016-07-31 21:54:53 -0700

    Add js code to submit voting page via ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-
   // Place JavaScript code here...
   $('.carousel').carousel({
   	keyboard: true,
     interval: 5000 //changes the speed
   });
   
-  var $votingForm = $('#voting-form');
+  var $votingPage = $('.voting-page');
+  var $votingForm = $votingPage.find('#voting-form');
   var $votingCards = $votingForm.find('.voting-card');
+  var $votingAlert = $votingPage.find('.voting-alert');
 
   $votingCards.click(function(event) {
+    // Update choice input value
     var chooseVal = $(this).attr('data-choose');
     $votingForm.find('input[name="choice"]').val(chooseVal);
 
@@ -17,15 +19,21 @@ $(document).ready(function() {
     $votingForm.find('i.fa.fa-star').removeClass('selected');
     $(this).find('i.fa.fa-star').addClass('selected');
     
-    //$votingForm.submit();
-
+    // Submit via ajax
     $.ajax({
       method: 'POST',
       url: '/api/choose',
       data: $('form').serializeToJSON()
-    }).done(function(msg) {
-      console.log(msg);
+    }).done(function(response) {
+      response = response || {};
+      $votingAlert.removeClass('hide');
+      $votingAlert.find('.voting-message').text(response.message);
     });
     
   });
+
+  $votingAlert.find('button').click(function(event) {
+    $votingAlert.addClass('hide');
+  });
+
 });
